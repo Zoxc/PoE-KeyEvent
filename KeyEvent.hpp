6f5ef28a77a185e67b5ebd14221fb1f4ee872f6b
Zoxc <zoxc32@gmail.com>
2013-02-11 06:36:48 +0100

    Sends the shared secret.

--- KeyEvent.hpp
+++ KeyEvent.hpp
@@ -3,9 +3,8 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <string>
-#include <sstream>
-#include <fstream>
 #include <Evntprov.h>
 #include "Manifest\ETW.h"
+#include "sha4.h"
 
 const void *find_pattern(HANDLE module, std::string name, const char *pattern, const char *mask, size_t size);
